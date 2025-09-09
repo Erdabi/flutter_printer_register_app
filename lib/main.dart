3db9b6b38cade5e7d521b6d9e7cf0a61f598d64d
Erdabi <yusuf.khurshid@gmx.ch>
2025-09-09 14:57:57 +0200

    main.dart edited

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
+import 'ui/home_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const PosApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class PosApp extends StatelessWidget {
+  const PosApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'POS Druck & Drawer',
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo),
+      home: const HomePage(),
     );
   }
 }
